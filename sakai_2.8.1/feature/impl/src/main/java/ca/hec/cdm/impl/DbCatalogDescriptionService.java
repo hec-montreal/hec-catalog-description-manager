773b0339e0e0f1208c02a562a59c499307ad13bb
Curtis van Osch <curtis.van-osch@hec.ca>
2012-05-25 13:17:15 +0000

    Injection du CatalogDescriptionService bean dans le EntityProvider

--- sakai_2.8.1/feature/impl/src/main/java/ca/hec/cdm/impl/DbCatalogDescriptionService.java
+++ sakai_2.8.1/feature/impl/src/main/java/ca/hec/cdm/impl/DbCatalogDescriptionService.java
@@ -2,4 +2,43 @@ package ca.hec.cdm.impl;
 
 public class DbCatalogDescriptionService extends BaseCatalogDescriptionService {
 
+	
+	/**
+	 * Final initialization, once all dependencies are set.
+	 */
+	public void init()
+	{
+//		try
+//		{
+//			// if we are auto-creating our schema, check and create
+//			if (m_autoDdl)
+//			{
+//				m_sqlService.ddl(this.getClass().getClassLoader(), "sakai_announcement");
+//			}
+//
+//			super.init();
+//
+//			M_log.info("init(): tables: " + m_cTableName + " " + m_rTableName + " locks-in-db: " + m_locksInDb);
+//
+//			// convert draft?
+//			if (m_convertToDraft)
+//			{
+//				m_convertToDraft = false;
+//				convertToDraft();
+//			}
+//
+//			// convert pubview?
+//			if (m_convertToPubView)
+//			{
+//				m_convertToPubView = false;
+//				convertToPubView();
+//			}
+//
+//		}
+//		catch (Throwable t)
+//		{
+//			M_log.warn("init(): ", t);
+//		}
+	}
+	
 }
