7536090cf2b4fbac00bb09fafe93f553c4fba001
Curtis van Osch <curtis.van-osch@hec.ca>
2012-05-29 20:11:51 +0000

    use custom actions and catalogDescriptionService

--- sakai_2.8.1/feature/tool/src/main/java/ca/hec/cdm/entityprovider/CatalogDescriptionEntityProviderImpl.java
+++ sakai_2.8.1/feature/tool/src/main/java/ca/hec/cdm/entityprovider/CatalogDescriptionEntityProviderImpl.java
@@ -23,23 +23,32 @@ package ca.hec.cdm.entityprovider;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import lombok.Data;
+import lombok.Setter;
 
+import org.apache.commons.lang.StringUtils;
 import org.sakaiproject.entitybroker.EntityReference;
+import org.sakaiproject.entitybroker.EntityView;
 import org.sakaiproject.entitybroker.entityprovider.CoreEntityProvider;
+import org.sakaiproject.entitybroker.entityprovider.annotations.EntityCustomAction;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.ActionsExecutable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.CollectionResolvable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Resolvable;
 import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
-import org.sakaiproject.entitybroker.entityprovider.search.Search;
 import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
 
+import ca.hec.cdm.api.CatalogDescriptionService;
+
 public class CatalogDescriptionEntityProviderImpl extends AbstractEntityProvider 
-	implements CoreEntityProvider, AutoRegisterEntityProvider, Resolvable, CollectionResolvable, Outputable {
+	implements CoreEntityProvider, AutoRegisterEntityProvider, Resolvable, Outputable, ActionsExecutable {
 
 	public final static String ENTITY_PREFIX = "catalog-description";
+	
+	@Setter 
+	private CatalogDescriptionService catalogDescriptionService;
 	
 	public String getEntityPrefix() {
 		return ENTITY_PREFIX;
@@ -49,15 +58,56 @@ public class CatalogDescriptionEntityProviderImpl extends AbstractEntityProvider
 		return new String[] { Formats.JSON };
 	}
 
-	public List<?> getEntities(EntityReference ref, Search search) {
-		List<DecoratedCatalogDescription> decoratedCatalogDescriptions = new ArrayList<DecoratedCatalogDescription>();
+	@EntityCustomAction(action="department", viewKey=EntityView.VIEW_LIST)
+	public List<?> getCatalogDescriptionsByDepartment(EntityView view, Map<String, Object> params) {
+
+		// get department
+		String department = view.getPathSegment(2);
+
+		//check that department is supplied
+		if (StringUtils.isBlank(department)) {
+			throw new IllegalArgumentException("Department must be set in order to get the catalog descriptions via the URL /catalog-description/department/departmentId");
+		}
+
+		List<SimpleCatalogDescription> decoratedCatalogDescriptions = new ArrayList<SimpleCatalogDescription>();
 		
+		String test = catalogDescriptionService.getLabel();
 		//convert raw CatalogDescriptions into decorated catalog descriptions
 		for (int i = 0; i < 5; i++)
 		{
-			DecoratedCatalogDescription dcd = 
-					new DecoratedCatalogDescription("Catalog Description " + i, "Teh descriptions!!");
-			dcd.setRequirements("these are teh requirements");
+			SimpleCatalogDescription dcd = 
+					new SimpleCatalogDescription("Catalog Description " + i, "La description du plan de cours annuaire");
+			dcd.setRequirements("label: " + test );
+			dcd.setDepartment(department);
+			dcd.setCareer("career");
+			decoratedCatalogDescriptions.add(dcd);
+		}
+
+		return decoratedCatalogDescriptions;
+	}
+
+	@EntityCustomAction(action="program", viewKey=EntityView.VIEW_LIST)
+	public List<?> getCatalogDescriptionsByProgram(EntityView view, Map<String, Object> params) {
+
+		// get program
+		String career = view.getPathSegment(2);
+
+		//check that department is supplied
+		if (StringUtils.isBlank(career)) {
+			throw new IllegalArgumentException("Program must be set in order to get the catalog descriptions via the URL /catalog-description/program/programId");
+		}
+
+		List<SimpleCatalogDescription> decoratedCatalogDescriptions = new ArrayList<SimpleCatalogDescription>();
+		
+		String test = catalogDescriptionService.getLabel();
+		//convert raw CatalogDescriptions into decorated catalog descriptions
+		for (int i = 0; i < 5; i++)
+		{
+			SimpleCatalogDescription dcd = 
+					new SimpleCatalogDescription("Catalog Description " + i, "La description du plan de cours annuaire");
+			dcd.setRequirements("label: " + test );
+			dcd.setDepartment("department");
+			dcd.setCareer(career);
 			decoratedCatalogDescriptions.add(dcd);
 		}
 
@@ -72,20 +122,21 @@ public class CatalogDescriptionEntityProviderImpl extends AbstractEntityProvider
 		return false;
 	}
 	
+	/**
+	 * Class to hold the subset of the fields that we want to display 
+	 */
 	@Data
-	public class DecoratedCatalogDescription implements Comparable<Object>{
+	public class SimpleCatalogDescription implements Comparable<Object>{
 		private final String title;
 		private final String description;
 		
+		private String department;
+		private String career;
 		private String requirements;
 		
 		public int compareTo(Object o) {
 			// TODO Auto-generated method stub
-			// sort order by 
 			return 0;
 		}
-		
-	
 	}
-
 }
