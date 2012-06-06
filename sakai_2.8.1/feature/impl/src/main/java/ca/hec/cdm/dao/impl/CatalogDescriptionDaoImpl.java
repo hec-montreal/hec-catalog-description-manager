c8bc51436122a6b0aac4c7b74339a5f524d2e0f2
Curtis van Osch <curtis.van-osch@hec.ca>
2012-06-06 15:19:46 +0000

    update service and dao

--- sakai_2.8.1/feature/impl/src/main/java/ca/hec/cdm/dao/impl/CatalogDescriptionDaoImpl.java
+++ sakai_2.8.1/feature/impl/src/main/java/ca/hec/cdm/dao/impl/CatalogDescriptionDaoImpl.java
@@ -1,35 +1,49 @@
 package ca.hec.cdm.dao.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
-//import org.apache.commons.logging.Log;
-//import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+
+import org.springframework.dao.DataAccessException;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-//import ca.hec.cdm.dao.CatalogDescriptionDao;
+import ca.hec.cdm.api.CatalogDescriptionDao;
 import ca.hec.cdm.model.CatalogDescription;
 
 
-public class CatalogDescriptionDaoImpl extends HibernateDaoSupport 
-//implements CatalogDescriptionDao 
+public class CatalogDescriptionDaoImpl extends HibernateDaoSupport implements CatalogDescriptionDao 
 {
 
-//    private static Log log = LogFactory.getLog(CatalogDescriptionDaoImpl.class);
-
-	public List<CatalogDescription> getCatalogDescriptions() {
-		
-		List<CatalogDescription> catalogDescriptions = new ArrayList<CatalogDescription>();
-		CatalogDescription cd = new CatalogDescription();
-		cd.setTitle("le titre du cd");
-		
-		catalogDescriptions.add(cd);
-		return catalogDescriptions;
-	}
+    private static Log log = LogFactory.getLog(CatalogDescriptionDaoImpl.class);
 
 	public void init() {
 		// TODO Auto-generated method stub
+	}
+
+	public CatalogDescription getCatalogDescription(Long id) {
+		return (CatalogDescription)getHibernateTemplate().get(CatalogDescription.class, id);
+	}
+	
+	public List<CatalogDescription> getCatalogDescriptionsByDepartment(String department) {
+		DetachedCriteria dc = DetachedCriteria.forClass(CatalogDescription.class)
+				.add(Restrictions.eq("department", department));
 		
+		return getHibernateTemplate().findByCriteria(dc);
+	}
+
+	public boolean saveCatalogDescription(CatalogDescription cd) {
+		try{
+			getHibernateTemplate().saveOrUpdate(cd);
+			return true;
+		}
+		catch (DataAccessException e) {
+			e.printStackTrace();
+			log.error("Hibernate could not save: " + e.toString());
+			return false;
+		}
 	}
 
 }
--- sakai_2.8.1/feature/impl/src/main/java/ca/hec/cdm/impl/CatalogDescriptionServiceImpl.java
+++ sakai_2.8.1/feature/impl/src/main/java/ca/hec/cdm/impl/CatalogDescriptionServiceImpl.java
@@ -1,38 +1,33 @@
 package ca.hec.cdm.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Setter;
 
-import org.sakaiproject.tool.api.SessionManager;
-
+import ca.hec.cdm.api.CatalogDescriptionDao;
 import ca.hec.cdm.api.CatalogDescriptionService;
-import ca.hec.cdm.dao.CatalogDescriptionDao;
 import ca.hec.cdm.model.CatalogDescription;
 
 public class CatalogDescriptionServiceImpl implements CatalogDescriptionService {
-
-	// beans to be injected by spring need a setter
-	@Setter	private SessionManager sessionManager;
-	@Setter	private CatalogDescriptionDao catalogDescriptionDao;
+	@Setter
+	private CatalogDescriptionDao catalogDescriptionDao;
 	
-	public List getCatalogDescriptions() {
-		
-		List catalogDescriptions = new ArrayList<CatalogDescription>();
-		
-		CatalogDescription cd = new CatalogDescription();
-		
-		cd.setTitle("Le Titre du cours");
-		cd.setDescription("La description du cours");
-		
-		catalogDescriptions.add(cd);
-		
-		return catalogDescriptions;
-	}
-
 	public void init() {
 		// TODO Auto-generated method stub
+	}
+
+	public CatalogDescription getCatalogDescription(Long id) {
+		return catalogDescriptionDao.getCatalogDescription(id);
+	}
+	
+	public List getCatalogDescriptionsByDepartment(String department) {
+		return catalogDescriptionDao.getCatalogDescriptionsByDepartment(department);
+	}
+	
+	public boolean updateDescription(Long id, String description) {
+		CatalogDescription cd = getCatalogDescription(id);
+		cd.setDescription(description);
 		
+		return catalogDescriptionDao.saveCatalogDescription(cd);
 	}
 }
