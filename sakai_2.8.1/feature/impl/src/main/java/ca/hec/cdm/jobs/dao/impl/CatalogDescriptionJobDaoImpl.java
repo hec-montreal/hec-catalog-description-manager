03c981d4bba99bb322fc8c36980a23e8a4b21947
Cyril Mace <cyril.mace@hec.ca>
2012-07-09 21:20:27 +0000

    Job de création des coquilles vides

---
+++ sakai_2.8.1/feature/impl/src/main/java/ca/hec/cdm/jobs/dao/impl/CatalogDescriptionJobDaoImpl.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * $Id: $
+ ******************************************************************************
+ *
+ * Copyright (c) 2012 The Sakai Foundation, The Sakai Quebec Team.
+ *
+ * Licensed under the Educational Community License, Version 1.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package ca.hec.cdm.jobs.dao.impl;
+
+import java.util.List;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import ca.hec.cdm.jobs.CatalogDescriptionJobDao;
+import ca.hec.cdm.jobs.model.CourseOffering;
+
+/**
+ *
+ * @author <a href="mailto:mathieu.cantin@hec.ca">Mathieu Cantin</a>
+ * @version $Id: $
+ */
+public class CatalogDescriptionJobDaoImpl  extends HibernateDaoSupport implements CatalogDescriptionJobDao {
+
+    public List<CourseOffering> getListCourseOffering() {
+	return (List<CourseOffering>) getHibernateTemplate().find("from CourseOffering where acad_career='APRE'");
+		
+    }
+    
+    
+
+}
+
