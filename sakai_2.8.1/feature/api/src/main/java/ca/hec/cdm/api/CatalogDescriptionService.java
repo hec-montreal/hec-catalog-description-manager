a2b0a0a85e6b089cfbe91700c4d708789b94048a
Cyril Mace <cyril.mace@hec.ca>
2012-07-17 20:55:39 +0000

    ZCGH-42: Terminer les services web et les brancher dans prototype

--- sakai_2.8.1/feature/api/src/main/java/ca/hec/cdm/api/CatalogDescriptionService.java
+++ sakai_2.8.1/feature/api/src/main/java/ca/hec/cdm/api/CatalogDescriptionService.java
@@ -8,61 +8,74 @@ import ca.hec.cdm.model.CatalogDescription;
 
 //import org.sakaiproject.entity.api.EntityProducer;
 
-public interface CatalogDescriptionService //extends EntityProducer 
+public interface CatalogDescriptionService // extends EntityProducer
 {
-	public void init();
-	
-	/**
-	 * Get a Catalog Description by database id
-	 *
-	 * @param id - the db id of the catalog description
-	 *
-	 * @return the catalog description with details
-	 */
-	public CatalogDescription getCatalogDescription(Long id);
-	
-	/**
-	 * Get a Catalog Description by course id
-	 *
-	 * @param id - the id of the catalog description
-	 *
-	 * @return the catalog description with details
-	 */
-	public CatalogDescription getCatalogDescription(String course_id);
+    public void init();
 
-	/**
-	 * Access a list of catalog descriptions by career
-	 * 
-	 * @param career - the career to search on
-	 *        
-	 * @return A list of catalog description objects that meet the criteria; may be empty
-	 */
-	public List<CatalogDescription> getCatalogDescriptionsByCareer(String career);
-	
-	/**
-	 * Access a list of catalog descriptions by department
-	 * 
-	 * @param department - the department to search on
-	 *        
-	 * @return A list of catalog description objects that meet the criteria; may be empty
-	 */
-	public List<CatalogDescription> getCatalogDescriptionsByDepartment(String department);
-	
-	/**
-	 * Update a CatalogDescription's description in the database
-	 * 
-	 * @param cd - the catalog description to update
-	 * @param description - the description to use for the update       
-	 * @throws StaleDataException 
-	 * @throws DatabaseException 
-	 */
-	public void updateDescription(CatalogDescription cd) throws StaleDataException, DatabaseException;
+    /**
+     * Get a Catalog Description by database id
+     * 
+     * @param id - the db id of the catalog description
+     * @return the catalog description with details
+     */
+    public CatalogDescription getCatalogDescription(Long id);
 
-	/**
-	 * Return whether or not the specified catalog description (by course_id) exists
-	 * 
-	 * @param course_id - the course id of the catalog description to update
-	 * @return true if it exists, false otherwise
-	 */
-	public boolean descriptionExists(String course_id);
+    /**
+     * Get a Catalog Description by course id
+     * 
+     * @param id - the id of the catalog description
+     * @return the catalog description with details
+     */
+    public CatalogDescription getCatalogDescription(String course_id);
+
+    /**
+     * Access a list of catalog descriptions by career
+     * 
+     * @param career - the career to search on
+     * @return A list of catalog description objects that meet the criteria; may
+     *         be empty
+     */
+    public List<CatalogDescription> getCatalogDescriptionsByCareer(String career);
+
+    /**
+     * Access a list of catalog descriptions by department
+     * 
+     * @param department - the department to search on
+     * @return A list of catalog description objects that meet the criteria; may
+     *         be empty
+     */
+    public List<CatalogDescription> getCatalogDescriptionsByDepartment(
+	    String department);
+
+    /**
+     * Get all departments that have at least one catalog description with an
+     * empty description
+     */
+    public List<String> getDepartmentNameWithAtLeastOneCaWithNoDescription();
+
+    /**
+     * Get all departments that have at least one catalog description with an
+     * empty description
+     */
+    public List<String> getCareerNameWithAtLeastOneCaWithNoDescription();
+
+    /**
+     * Update a CatalogDescription's description in the database
+     * 
+     * @param cd - the catalog description to update
+     * @param description - the description to use for the update
+     * @throws StaleDataException
+     * @throws DatabaseException
+     */
+    public void updateDescription(CatalogDescription cd)
+	    throws StaleDataException, DatabaseException;
+
+    /**
+     * Return whether or not the specified catalog description (by course_id)
+     * exists
+     * 
+     * @param course_id - the course id of the catalog description to update
+     * @return true if it exists, false otherwise
+     */
+    public boolean descriptionExists(String course_id);
 }
--- sakai_2.8.1/feature/tool/src/main/java/ca/hec/cdm/entityprovider/CatalogDescriptionEntityProviderImpl.java
+++ sakai_2.8.1/feature/tool/src/main/java/ca/hec/cdm/entityprovider/CatalogDescriptionEntityProviderImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import lombok.Data;
 import lombok.Setter;
 
 import org.apache.commons.lang.StringUtils;
@@ -22,100 +21,105 @@ import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
 
 import ca.hec.cdm.api.CatalogDescriptionService;
 import ca.hec.cdm.model.CatalogDescription;
+import ca.hec.cdm.model.SimpleCatalogDescription;
 
-public class CatalogDescriptionEntityProviderImpl extends AbstractEntityProvider 
-	implements CoreEntityProvider, AutoRegisterEntityProvider, Resolvable, Sampleable, Outputable, ActionsExecutable {
+public class CatalogDescriptionEntityProviderImpl extends
+	AbstractEntityProvider implements CoreEntityProvider,
+	AutoRegisterEntityProvider, Resolvable, Sampleable, Outputable,
+	ActionsExecutable {
 
-	public final static String ENTITY_PREFIX = "catalog-description";
-	
-	// should this be the proxy?
-	@Setter 
-	private CatalogDescriptionService catalogDescriptionService;
-	
-	public String getEntityPrefix() {
-		return ENTITY_PREFIX;
+    public final static String ENTITY_PREFIX = "catalogDescription";
+
+    // should this be the proxy?
+    @Setter
+    private CatalogDescriptionService catalogDescriptionService;
+
+    public String getEntityPrefix() {
+	return ENTITY_PREFIX;
+    }
+
+    public String[] getHandledOutputFormats() {
+	return new String[] { Formats.JSON };
+    }
+
+    @EntityCustomAction(action = "getCatalogDescriptionsByDepartment", viewKey = EntityView.VIEW_LIST)
+    public List<?> getCatalogDescriptionsByDepartment(EntityView view,
+	    Map<String, Object> params) {
+
+	String department = view.getPathSegment(2);
+
+	// check that department is supplied
+	if (StringUtils.isBlank(department)) {
+	    throw new IllegalArgumentException(
+		    "Department must be set in order to get the catalog descriptions via the URL /catalog-description/department/departmentId");
 	}
 
-	public String[] getHandledOutputFormats() {
-		return new String[] { Formats.JSON };
+	return simplifyCatalogDescriptions(catalogDescriptionService
+		.getCatalogDescriptionsByDepartment(department));
+    }
+
+    @EntityCustomAction(action = "getCatalogDescriptionsByCareer", viewKey = EntityView.VIEW_LIST)
+    public List<?> getCatalogDescriptionsByCareer(EntityView view,
+	    Map<String, Object> params) {
+
+	String career = view.getPathSegment(2);
+
+	// check that department is supplied
+	if (StringUtils.isBlank(career)) {
+	    throw new IllegalArgumentException(
+		    "Career must be set in order to get the catalog descriptions via the URL /catalog-description/career/careerId");
 	}
 
-	@EntityCustomAction(action="department", viewKey=EntityView.VIEW_LIST)
-	public List<?> getCatalogDescriptionsByDepartment(EntityView view, Map<String, Object> params) {
+	return simplifyCatalogDescriptions(catalogDescriptionService
+		.getCatalogDescriptionsByCareer(career));
+    }
 
-		String department = view.getPathSegment(2);
+    @EntityCustomAction(action = "getDepartments", viewKey = EntityView.VIEW_LIST)
+    public List<?> getDepartments(EntityView view, Map<String, Object> params) {
 
-		//check that department is supplied
-		if (StringUtils.isBlank(department)) {
-			throw new IllegalArgumentException("Department must be set in order to get the catalog descriptions via the URL /catalog-description/department/departmentId");
-		}
+	return catalogDescriptionService
+		.getDepartmentNameWithAtLeastOneCaWithNoDescription();
+    }
 
-		return simplifyCatalogDescriptions(
-				catalogDescriptionService.getCatalogDescriptionsByDepartment(department));
+    @EntityCustomAction(action = "getCareers", viewKey = EntityView.VIEW_LIST)
+    public List<?> getCareers(EntityView view, Map<String, Object> params) {
+
+	return catalogDescriptionService
+		.getCareerNameWithAtLeastOneCaWithNoDescription();
+    }
+
+    public Object getEntity(EntityReference ref) {
+	return catalogDescriptionService.getCatalogDescription(ref.getId());
+    }
+
+    public boolean entityExists(String course_id) {
+	return catalogDescriptionService.descriptionExists(course_id);
+    }
+
+    public List<SimpleCatalogDescription> simplifyCatalogDescriptions(
+	    List<CatalogDescription> catalogDescriptions) {
+	List<SimpleCatalogDescription> simpleCatalogDescriptions =
+		new ArrayList<SimpleCatalogDescription>();
+
+	// convert raw CatalogDescriptions into decorated catalog descriptions
+	for (CatalogDescription cd : catalogDescriptions) {
+	    SimpleCatalogDescription scd = new SimpleCatalogDescription();
+
+	    scd.setTitle(cd.getTitle());
+	    scd.setDescription(cd.getDescription());
+	    scd.setDepartment(cd.getDepartment());
+	    scd.setCareer(cd.getCareer());
+	    scd.setRequirements(cd.getRequirements());
+	    scd.setCourseId(cd.getCourseId());
+	    scd.setCredits("" + cd.getCredits());
+
+	    simpleCatalogDescriptions.add(scd);
 	}
 
-	@EntityCustomAction(action="career", viewKey=EntityView.VIEW_LIST)
-	public List<?> getCatalogDescriptionsByCareer(EntityView view, Map<String, Object> params) {
+	return simpleCatalogDescriptions;
+    }
 
-		String career = view.getPathSegment(2);
-
-		//check that department is supplied
-		if (StringUtils.isBlank(career)) {
-			throw new IllegalArgumentException("Career must be set in order to get the catalog descriptions via the URL /catalog-description/career/careerId");
-		}
-
-		return simplifyCatalogDescriptions(
-				catalogDescriptionService.getCatalogDescriptionsByCareer(career));
-	}
-
-	public Object getEntity(EntityReference ref) {
-		return catalogDescriptionService.getCatalogDescription(ref.getId());
-	}
-
-	public boolean entityExists(String course_id) {
-		return catalogDescriptionService.descriptionExists(course_id);
-	}
-	
-	public List<SimpleCatalogDescription> simplifyCatalogDescriptions(List<CatalogDescription> catalogDescriptions) {
-		List<SimpleCatalogDescription> simpleCatalogDescriptions = new ArrayList<SimpleCatalogDescription>();
-
-		//convert raw CatalogDescriptions into decorated catalog descriptions
-		for (CatalogDescription cd : catalogDescriptions)
-		{
-			SimpleCatalogDescription scd = 
-					new SimpleCatalogDescription();
-						
-			scd.setTitle(cd.getTitle());
-			scd.setDescription(cd.getDescription());
-			scd.setDepartment(cd.getDepartment());
-			scd.setCareer(cd.getCareer());
-			scd.setRequirements(cd.getRequirements());
-			
-			simpleCatalogDescriptions.add(scd);
-		}
-
-		return simpleCatalogDescriptions;
-	}
-	
-	/**
-	 * Class to hold the subset of the fields that we want to display 
-	 */
-	@Data
-	public class SimpleCatalogDescription implements Comparable<Object>{
-		private String title;
-		private String description;
-		
-		private String department;
-		private String career;
-		private String requirements;
-		
-		public int compareTo(Object o) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-	}
-
-	public Object getSampleEntity() {
-		return new SimpleCatalogDescription();
-	}
+    public Object getSampleEntity() {
+	return new SimpleCatalogDescription();
+    }
 }
