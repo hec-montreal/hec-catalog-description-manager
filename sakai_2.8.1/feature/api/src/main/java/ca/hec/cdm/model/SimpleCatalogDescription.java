96f899b8dc8f7c758ce183d5250a693a276197fd
Cyril Mace <cyril.mace@hec.ca>
2012-08-02 15:56:56 +0000

    ZCGH-42: Terminer les services web et les brancher dans prototype - filtre par langue

--- sakai_2.8.1/feature/api/src/main/java/ca/hec/cdm/model/SimpleCatalogDescription.java
+++ sakai_2.8.1/feature/api/src/main/java/ca/hec/cdm/model/SimpleCatalogDescription.java
@@ -16,6 +16,7 @@ public class SimpleCatalogDescription implements Comparable<Object> {
 	private String career;
 	private String requirements;
 	private String credits;
+	private String lang;
 	private String specificCourse;
 
 	public int compareTo(Object o) {
