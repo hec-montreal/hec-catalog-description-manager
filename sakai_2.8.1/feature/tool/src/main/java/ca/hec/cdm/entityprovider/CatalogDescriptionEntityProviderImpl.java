92b16128bd1a8c3e3dd517115a8cbb1fe2d9b936
Curtis van Osch <curtis.van-osch@hec.ca>
2012-08-06 15:18:45 +0000

    handle case where language is null more gracefully

--- sakai_2.8.1/feature/tool/src/main/java/ca/hec/cdm/entityprovider/CatalogDescriptionEntityProviderImpl.java
+++ sakai_2.8.1/feature/tool/src/main/java/ca/hec/cdm/entityprovider/CatalogDescriptionEntityProviderImpl.java
@@ -127,7 +127,10 @@ public class CatalogDescriptionEntityProviderImpl extends AbstractEntityProvider
 	scd.setRequirements(cd.getRequirements());
 	scd.setCourseId(cd.getCourseId());
 	scd.setCredits("" + cd.getCredits());
-	scd.setLang(cd.getLanguage().substring(0, 2));
+	
+	String lang = cd.getLanguage();
+	if (lang != null)
+	    scd.setLang(lang.substring(0, 2));
 	    
 	// TODO: make this conditional, only if catalog description didn't have one in the db
 	scd.setSpecificCourse(sakaiProxy.getSpecificCourse(cd.getCourseId()));
